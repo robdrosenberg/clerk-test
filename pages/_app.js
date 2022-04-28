6d8b7660d7f68260ba1506f0616b9e9768309758
Robert Rosenberg <robdrosenberg@gmail.com>
2022-04-28 10:32:57 -0700

    clerk initial setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import '../styles/globals.css'
+import { ClerkProvider } from '@clerk/nextjs';
+import { useRouter } from "next/router";
+
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	const { push } = useRouter();
+
+  return (
+	<ClerkProvider 
+		// frontendApi={process.env.NEXT_PUBLIC_CLERK_FRONTEND_API}
+		// navigate={(to) => push(to)}
+		{...pageProps}
+	>
+		<Component {...pageProps} />
+	</ClerkProvider>
+  )
 }
 
 export default MyApp
